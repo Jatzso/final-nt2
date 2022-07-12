7d67d5498bb4b57affb3b56cf819fda857142e4f
Jatzso <91919512+Jatzso@users.noreply.github.com>
2022-07-12 20:25:26 -0300

    Final resuelto

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Funcionalidad />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Funcionalidad from './components/Funcionalidad.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   Funcionalidad
   }
 }
 </script>
---
+++ src/components/Funcionalidad.vue
@@ -0,0 +1,80 @@
+<template>
+    <div>
+  <label for="color">Color:</label>
+  <input type="text" v-model="color">
+  <label for="tamanio">Tamanio:</label>
+  <input type="text" v-model="tamanio">
+  <label for="fondo">Fondo:</label>
+  <input type="text" v-model="fondo">
+  <label for="texto" style="margin-top:10px">Texto:</label>
+  <input type="text" v-model="texto" :style="{color : this.color, backgroundColor: this.fondo, fontSize: this.tamanio + 'px'}">
+  <p>Cantidad de palabras: {{ contarPalabras }}</p>
+
+  <table>
+  <tr>
+    <th>Número de pregunta</th>
+    <th>Respuesta</th>
+    
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>b</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>c</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>b</td>
+  </tr>
+  <tr>
+    <td>4</td>
+    <td>d</td>
+  </tr>
+  <tr>
+    <td>5</td>
+    <td>d</td>
+  </tr>
+</table>
+ </div>
+
+</template>
+
+<script>
+
+  export default  {
+    name: 'src-components-funcionalidad',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+        texto:'',
+        color: '',
+        tamanio: '',
+        fondo: '',
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+      contarPalabras(){
+        return this.texto.trim().split(' ').length
+      }
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+  .src-components-funcionalidad {
+
+  }
+  table, th, td {
+  border:solid black;
+}
+</style>
